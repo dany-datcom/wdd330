4a4934a4b45f330ae1f0ef8c863a90076a107bd2
dany-datcom <djimenezgonzalez@byupathway.com>
2026-03-09 20:00:54 -0600

    product js

--- src/js/product.js
+++ src/js/product.js
@@ -1,18 +1,18 @@
-<<<<<<<<< Temporary merge branch 1
+
 import { setLocalStorage, getLocalStorage } from "./utils.mjs";
-=========
+
 import { getLocalStorage, setLocalStorage } from "./utils.mjs";
->>>>>>>>> Temporary merge branch 2
+
 import ProductData from "./ProductData.mjs";
 
 const dataSource = new ProductData("tents");
 
 function addProductToCart(product) {
-<<<<<<<<< Temporary merge branch 1
+
   let cartItems = getLocalStorage("so-cart") || [];
-=========
+
   const cartItems = getLocalStorage("so-cart") || [];
->>>>>>>>> Temporary merge branch 2
+
   cartItems.push(product);
   setLocalStorage("so-cart", cartItems);
 }
