0cb4db994ecbb93406bc5cf2052688ccb9d11886
dany-datcom <djimenezgonzalez@byupathway.edu>
2026-03-26 15:09:54 -0600

    week4

--- src/js/ExternalServices.mjs
+++ src/js/ExternalServices.mjs
@@ -19,12 +19,27 @@ async function fetchData(endpoint) {
   }
 }
 
-export default class ProductData {
+export default class ExternalServices {
   async getData(category) {
     return await fetchData(`products/search/${category}`);
   }
 
   async findProductById(id) {
     return await fetchData(`product/${id}`);
+  }
+
+  async checkout(payload) {
+    const url = "https://wdd330-backend.onrender.com/checkout";
+
+    const options = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(payload)
+    };
+
+    const response = await fetch(url, options);
+    return await response.json();
   }
 }
---
+++ src/js/checkoutProcess.mjs
@@ -0,0 +1,85 @@
+import ExternalServices from './ExternalServices.mjs';
+export default class CheckoutProcess {
+  constructor(key, outputSelector) {
+    this.key = key;
+    this.outputSelector = outputSelector;
+    this.list = [];
+    this.itemTotal = 0;
+    this.shipping = 0;
+    this.tax = 0;
+    this.orderTotal = 0;
+  }
+
+  init() {
+    this.list = JSON.parse(localStorage.getItem(this.key)) || [];
+    this.calculateItemSubTotal();
+  }
+
+  calculateItemSubTotal() {
+    this.itemTotal = this.list.reduce(
+      (sum, item) => sum + item.price * item.quantity,
+      0
+    );
+
+    document.querySelector('#subtotal').innerText =
+      `$${this.itemTotal.toFixed(2)}`;
+  }
+
+  calculateOrderTotal() {
+    // TAX 6%
+    this.tax = this.itemTotal * 0.06;
+
+    // SHIPPING
+    this.shipping = this.list.length > 0
+      ? 10 + (this.list.length - 1) * 2
+      : 0;
+
+    this.orderTotal = this.itemTotal + this.tax + this.shipping;
+
+    this.displayOrderTotals();
+  }
+
+  displayOrderTotals() {
+    document.querySelector('#tax').innerText =
+      `$${this.tax.toFixed(2)}`;
+
+    document.querySelector('#shipping').innerText =
+      `$${this.shipping.toFixed(2)}`;
+
+    document.querySelector('#total').innerText =
+      `$${this.orderTotal.toFixed(2)}`;
+  }
+
+  packageItems(items) {
+    return items.map(item => ({
+      id: item.id,
+      name: item.name,
+      price: item.price,
+      quantity: item.quantity
+    }));
+  }
+
+  async checkout(form) {
+    const formData = new FormData(form);
+    const data = {};
+
+    formData.forEach((value, key) => {
+      data[key] = value;
+    });
+
+    const order = {
+      ...data,
+      orderDate: new Date().toISOString(),
+      items: this.packageItems(this.list),
+      orderTotal: this.orderTotal.toFixed(2),
+      shipping: this.shipping,
+      tax: this.tax.toFixed(2)
+    };
+
+    const service = new ExternalServices();
+
+    const result = await service.checkout(order);
+
+    console.log('Respuesta del servidor:', result);
+  }
+}
